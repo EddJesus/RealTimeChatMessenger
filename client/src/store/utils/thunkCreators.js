3ae36aeeacad625fa8416b582531d682656c4b3a
EddJesus <edusanto22@gmail.com>
2021-05-12 00:35:47 -0300

    refactor: change localStorage to cookie

--- client/src/store/utils/thunkCreators.js
+++ client/src/store/utils/thunkCreators.js
@@ -8,8 +8,10 @@ import {
 } from "../conversations";
 import { gotUser, setFetchingStatus } from "../user";
 
+import CookieService from '../../services/CookieService';
+
 axios.interceptors.request.use(async function (config) {
-  const token = await localStorage.getItem("messenger-token");
+  const token = await CookieService.getCookie("messenger-token");
   config.headers["x-access-token"] = token;
 
   return config;
@@ -35,7 +37,7 @@ export const fetchUser = () => async (dispatch) => {
 export const register = (credentials) => async (dispatch) => {
   try {
     const { data } = await axios.post("/auth/register", credentials);
-    await localStorage.setItem("messenger-token", data.token);
+    await CookieService.setCookie("messenger-token", data.token)
     dispatch(gotUser(data));
     socket.emit("go-online", data.id);
   } catch (error) {
@@ -47,7 +49,7 @@ export const register = (credentials) => async (dispatch) => {
 export const login = (credentials) => async (dispatch) => {
   try {
     const { data } = await axios.post("/auth/login", credentials);
-    await localStorage.setItem("messenger-token", data.token);
+    await CookieService.setCookie("messenger-token", data.token)
     dispatch(gotUser(data));
     socket.emit("go-online", data.id);
   } catch (error) {
@@ -59,7 +61,7 @@ export const login = (credentials) => async (dispatch) => {
 export const logout = (id) => async (dispatch) => {
   try {
     await axios.delete("/auth/logout");
-    await localStorage.removeItem("messenger-token");
+    await CookieService.remove("messenger-token")
     dispatch(gotUser({}));
     socket.emit("logout", id);
   } catch (error) {
