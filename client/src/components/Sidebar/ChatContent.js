d6f345b012ca888edd292bc419eff0b0cde0b845
EddJesus <edusanto22@gmail.com>
2021-05-13 19:38:26 -0300

    fix: messages

--- client/src/components/Sidebar/ChatContent.js
+++ client/src/components/Sidebar/ChatContent.js
@@ -17,6 +17,10 @@ const useStyles = makeStyles((theme) => ({
     fontSize: 12,
     color: "#9CADC8",
     letterSpacing: -0.17,
+    maxWidth: "100%",
+    overflow: "hidden",
+    whiteSpace: "nowrap",
+    textOverflow: "ellipsis"
   },
   notification: {
     height: 20,
