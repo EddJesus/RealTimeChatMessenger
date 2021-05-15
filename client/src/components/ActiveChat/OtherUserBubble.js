1569ef90814c9393d6927079d05d3084bbaffeb7
EddJesus <edusanto22@gmail.com>
2021-05-15 13:16:12 -0300

    fix: PR issues

--- client/src/components/ActiveChat/OtherUserBubble.js
+++ client/src/components/ActiveChat/OtherUserBubble.js
@@ -21,7 +21,6 @@ const useStyles = makeStyles(() => ({
   bubble: {
     backgroundImage: "linear-gradient(225deg, #6CC1FF 0%, #3A8DFF 100%)",
     borderRadius: "0 10px 10px 10px",
-    maxWidth: "70%",
   },
   text: {
     fontSize: 14,
